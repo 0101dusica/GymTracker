e6b5e0ae632b5fdac2d3dff8fdadb39cef87c781
0101dusica <trbovicdusica@gmail.com>
2025-05-16 12:25:08 +0200

    Add DTOs for User and WorkoutSession

---
+++ GymTracker.Application/DTOs/Workout/WorkoutSessionDto.cs
@@ -0,0 +1,21 @@
+﻿using GymTracker.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTracker.Application.DTOs.Workout
+{
+    public class WorkoutSessionDto
+    {
+        public DateTime Timestamp { get; set; }
+        public ExerciseType Type { get; set; }
+        public int DurationMinutes { get; set; }
+        public int CaloriesBurned { get; set; }
+        public int Intensity { get; set; }
+        public int FatigueLevel { get; set; }
+        public string? Notes { get; set; }
+    }
+
+}
