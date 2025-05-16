7e3a7e149a20063677288ee732c7a42c4965a86d
0101dusica <trbovicdusica@gmail.com>
2025-05-16 14:25:42 +0200

    Add UserResponseDto

---
+++ GymTracker.Application/Mapping/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using GymTracker.Application.DTOs.User;
+using GymTracker.Core.Entities;
+using AutoMapper;
+
+namespace GymTracker.Application.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserResponseDto>();
+            CreateMap<UserRegisterDto, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore())           
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore());
+        }
+    }
+}
---
+++ GymTracker.Application/Mapping/WorkoutProfile.cs
@@ -0,0 +1,14 @@
+﻿using GymTracker.Application.DTOs.Workout;
+using GymTracker.Core.Entities;
+using AutoMapper;
+
+namespace GymTracker.Application.Mapping
+{
+    public class WorkoutProfile : Profile
+    {
+        public WorkoutProfile()
+        {
+            CreateMap<WorkoutSession, WorkoutSessionDto>().ReverseMap();
+        }
+    }
+}
