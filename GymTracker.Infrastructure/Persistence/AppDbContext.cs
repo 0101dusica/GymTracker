49459e64f0e1a0e17a895d11111f08d318f1848f
0101dusica <trbovicdusica@gmail.com>
2025-05-16 13:25:46 +0200

    Introduced main database class for SQLite integration

---
+++ GymTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using GymTracker.Core.Entities;
+using System.Reflection.Emit;
+
+namespace GymTracker.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<WorkoutSession> WorkoutSessions => Set<WorkoutSession>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.WorkoutSessions)
+                .WithOne(w => w.User)
+                .HasForeignKey(w => w.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
