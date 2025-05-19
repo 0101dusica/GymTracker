1db486d0c3aba6f1bdf686f7040c051a064103b6
0101dusica <trbovicdusica@gmail.com>
2025-05-19 13:56:25 +0200

    Add auth guard with localStorage check

--- GymTracker.Client/src/app/authModule/guards/auth-required-guard.guard.ts
+++ GymTracker.Client/src/app/authModule/guards/auth-required-guard.guard.ts
@@ -1,12 +1,13 @@
 import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const authRequiredGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    return true;
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) return true;
   }
 
-  const router = new Router();
+  const router = inject(Router);
   router.navigate(['/login']);
   return false;
 };
