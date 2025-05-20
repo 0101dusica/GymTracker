db9d35f51b8727a19bf1f25ea421e41632402b3e
0101dusica <trbovicdusica@gmail.com>
2025-05-20 18:52:50 +0200

    Create account page for viewing and updating user details

--- GymTracker.Client/src/app/userModule/user.service.ts
+++ GymTracker.Client/src/app/userModule/user.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getCurrentUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/me`);
+  }
+
+  updateUser(user: User): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}`, user);
+  }
+  
 }
